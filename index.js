5e37be01d8b2eca3a61f654fef120eb29c9b575b
Macarena Cuevas Guzmán <macarenacuevasg@gmail.com>
2020-11-16 16:51:07 -0300

    argv corregidos

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mdlinks = require('./md-links.js');
 const path = require('path');
 
-let pathRequire = process.argv[2]
+let pathRequire = process.argv[1]
 pathRequire = path.resolve(pathRequire);
 
-const option1 = process.argv[3];
-const option2 = process.argv[4];
+const option1 = process.argv[2];
+const option2 = process.argv[3];
 
 mdlinks(pathRequire,option1,option2)
